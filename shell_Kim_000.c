137b01095c5c5e47e813be1e6ff664ac63c01816
kimpham1234 <pghkim94@gmail.com>
2017-02-26 16:29:20 -0800

    fix exit

--- shell_Kim_000.c
+++ shell_Kim_000.c
@@ -10,137 +10,98 @@
 #include <unistd.h>
 #include <time.h>
 #include <string.h>
+#include <stdlib.h>
 #define MAXLINE 80
 
-/*
-void split(char **array, char* input){
-   // char* array[100];
-    printf("Enter\n");
-    char* token;
-    const char delim[2] = " ";
-    
-    token = strtok(input, delim);
-    *token+='\0';
-    //array[0] = "hello";
-    array[0] = token+'\0';
-    printf("token %s\n", token);
-    printf("array %s\n", array[0]);
-    
-   
-    int i = 1;
-    while(token!=NULL){
-        token = strtok(NULL, delim);
-        if(token!=NULL)
-            *token+='\0';
-        
-      //  array[i] = "hi";
-        array[i] = token+'\0';
-
-        printf("token %s\n", token);
-        printf("array %s\n", array[i]);
-        
-        i++;
-    }
-    printf("Leave\n");
-    //return array;
-}*/
-/*
-char** split(char* input){
-    char* token;
-    const char delim[2] = " ";
-    int size = 1;
-    char** array = malloc(sizeof(char*) * size);
-    
-    if(array==NULL)
-        return NULL;
-    
-    token = strtok(input, delim);
-    array[0] = token;
-    
-    while(token!=NULL){
-        size++;
-        array = realloc(array, sizeof(char*) * size++);
-        token = strtok(NULL, input);
-        array[size] = token;
-    }
-    array = realloc(array, sizeof(char*) * size++);
-    array[size] = NULL;
-    return array;
-}
-*/
 int main(void) {
-    char* args[40];
+    char* args[MAXLINE/2 + 1];
     char input[100];
     char* token;
     const char delim[2] = " ";
-    int i = 1;
-
+    int background;
+    int status;
+    int result = 0;
+    
     int should_run = 1;
     printf("CS149 Shell from Kim Pham\n");
     
-    //fgets(input, 100, stdin);
-    //
     while(should_run){
         printf("CutieBoo-000>");
         fflush(stdout);
-        fgets(input, 100, stdin);
-        printf("%s", input);
         
-        //trim
+        //read user output
+        fgets(input, 100, stdin);
+        
+        //trim '\n' at the end
         int len =(int) strlen(input);
         input[len-1] = '\0';
         
-        //start spliting
+        //start spliting tokens
         token = strtok(input, delim);
         args[0] = token;
+        int i = 1;
         while(token){
             token = strtok(NULL, delim);
             args[i] = token;
             i++;
         }
-       //done spliting
+       //done spliting tokens
+        
+        printf("%s\n", args[0]);
+        printf("%s\n", args[1]);
 
-//        printf("%s\n",args[0]);
-//        printf("%s\n",args[1]);
-//        printf("%s\n",args[2]);
-  //      args[0] = "ls";
-  //      args[1] = "-l";
-  //      args[2] = NULL;
-        int result = execvp(args[0], args);
-        if(result == -1){
-            printf("Error\n");
+        
+        //check for exit command
+        if(strcmp(args[0], "exit")==0 || strcmp(args[0], "exit\n")==0){
+            printf("yesbgvgvh\n");
+            should_run = 0;
+        }else{
+            printf("No");
         }
         
-       
+        if(should_run==1){
+            //check for background process command
+            if(strcmp(args[i-2], "&")==0){
+                background = 1;
+                args[i-2] = NULL;
+            }else
+                background = 0;
+            
+            //fork a new process
+            int pid = fork();
+            
+            if(pid == -1){
+                printf("Error in fork");
+            }else if(pid == 0){ //child process
+                result = execvp(args[0], args);
+                if(result == -1){
+                    perror("error");
+                    exit(0);
+                }
+            }
+            else{ //parent
+                if(background == 0)
+                    waitpid(pid, &status, 0);
+            }
+        }
         
-        
-        
-        
-        
-        
-        
-        should_run--;
     }
-    
-    
-    //split stuff
-    
-   
-    
-    
-    
     return 0;
 }
+
+
 /*
- char* args[2];
- args[0] = argv[1];
- args[1] = NULL;
+ char* args[3];
+ args[0] = "sleep";
+ args[1] = "5m";
+ args[2] = NULL;
  
- 
- 
+ if(fork()==0){
  int result = execvp(args[0], args);
  if(result == -1){
  printf("Error");
+ }
+ }
  }
  */
 
